a1f9cc7367e71496c8144b818617f71d6739cfcc
Rio Arjuna <>
2021-03-16 19:07:17 +0700

    :tada: Initial commit.

---
+++ app/src/main/java/rio/arj/imovie/MainActivity.kt
@@ -0,0 +1,11 @@
+package rio.arj.imovie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
